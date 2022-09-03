7dba7860a71c4694e97ea2b6e386a02a75afc753
Kurumi-t <kurumisda@gamil.com>
2022-09-03 09:17:40 +0000

    [add]アソシエーション、バージョンの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :post_recipes, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :shopping_lists, dependent: :destroy
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
 end
