df65b909789c5bcee3a7e9e650a8748b53dcd96d
Kurumi-t <kurumisda@gamil.com>
2022-09-05 14:13:13 +0000

    [add]食材一覧画面、食材追加機能

--- app/controllers/admin/foods_controller.rb
+++ app/controllers/admin/foods_controller.rb
@@ -1,4 +1,24 @@
 class Admin::FoodsController < ApplicationController
+  def create
+    @food = Food.new(food_params)
+    @food.save
+    redirect_to admin_path
+  end
+
   def edit
+    @food = Food.find(params[:id])
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+  def food_params
+    params.permit(:food_name, :genre, :unit, :weight_per_unit)
   end
 end
