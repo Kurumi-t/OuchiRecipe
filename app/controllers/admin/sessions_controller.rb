6e52bb3621961c443d88f5886a222156355ee5c7
Kurumi-t <kurumisda@gamil.com>
2022-09-05 08:35:11 +0000

    [add]管理者ログイン機能

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -17,8 +17,14 @@ class Admin::SessionsController < Devise::SessionsController
   # def destroy
   #   super
   # end
+  protected
+    def after_sign_in_path_for(resource)
+      admin_path
+    end
 
-  # protected
+    def after_sign_out_path_for(resource)
+      new_admin_session_path
+    end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
