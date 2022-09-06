ab6d78e46a2afdc243be3d36ecfc3e8847beadcb
Kurumi-t <kurumisda@gamil.com>
2022-09-06 14:17:43 +0000

    [add]アソシエーション、画像、バリデーション設定

--- app/models/post_recipe.rb
+++ app/models/post_recipe.rb
@@ -1,2 +1,20 @@
 class PostRecipe < ApplicationRecord
+  belongs_to :user, optional: true
+  has_many :ingredients, dependent: :destroy
+  has_many :making_recipes, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+  has_one_attached :recipe_image
+  validates :title, presence: true
+  validates :advice, presence: true
+  validates :is_draft, presence: true
+
+  def get_recipe_image(width, height)
+    unless recipe_image.attached?
+      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      recipe_image.attach(io: File.open(file_path), filename: 'recipe-default-image.jpg', content_type: 'image/jpeg')
+    end
+    recipe_image.variant(resize_to_limit: [width, height]).processed
+  end
 end
