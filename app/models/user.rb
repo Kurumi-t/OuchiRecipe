6792e0890de15a543d72653eb0e12d8429840238
Kurumi-t <kurumisda@gamil.com>
2022-09-06 14:16:48 +0000

    [add]画像の設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,16 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :shopping_lists, dependent: :destroy
 
+  has_one_attached :profile_image
   validates :name, presence: true
   validates :email, presence: true
   validates :encrypted_password, presence: true
+
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'profile-default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
 end
