c9ebbc675595f3777c7a5af52a1096bdba2449b3
Kurumi-t <kurumisda@gamil.com>
2022-09-06 14:20:04 +0000

    [add]レシピ一覧投稿機能

--- app/controllers/public/post_recipes_controller.rb
+++ app/controllers/public/post_recipes_controller.rb
@@ -1,8 +1,10 @@
 class Public::PostRecipesController < ApplicationController
   def index
+    @post_recipes = PostRecipe.all
   end
 
   def new
+    @post_recipe = PostRecipe.new
   end
 
   def show
