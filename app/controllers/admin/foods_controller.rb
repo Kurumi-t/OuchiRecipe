d683ceb32cada716b6157ba46780a387c924eb95
Kurumi-t <kurumisda@gamil.com>
2022-09-06 11:40:32 +0000

    [add]食材投稿、編集、削除機能

--- app/controllers/admin/foods_controller.rb
+++ app/controllers/admin/foods_controller.rb
@@ -1,8 +1,13 @@
 class Admin::FoodsController < ApplicationController
+  def index
+    @food = Food.new
+    @foods = Food.all
+  end
+
   def create
     @food = Food.new(food_params)
     @food.save
-    redirect_to admin_path
+    redirect_to admin_foods_path
   end
 
   def edit
@@ -10,15 +15,19 @@ class Admin::FoodsController < ApplicationController
   end
 
   def update
-
+    @food = Food.find(params[:id])
+    @food.update(food_params)
+    redirect_to edit_admin_food_path(@food.id)
   end
 
   def destroy
-
+    @food = Food.find(params[:id])
+    @food.destroy
+    redirect_to admin_foods_path
   end
 
   private
   def food_params
-    params.permit(:food_name, :genre, :unit, :weight_per_unit)
+    params.require(:food).permit(:food_name, :genre, :unit, :weight_per_unit)
   end
 end
