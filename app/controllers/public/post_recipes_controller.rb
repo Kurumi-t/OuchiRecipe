5f82f0a7b69fc60595667754aaf73916d6e48176
Kurumi-t <kurumisda@gamil.com>
2022-09-07 11:34:59 +0000

    [add]レシピ投稿機能

--- app/controllers/public/post_recipes_controller.rb
+++ app/controllers/public/post_recipes_controller.rb
@@ -5,11 +5,45 @@ class Public::PostRecipesController < ApplicationController
 
   def new
     @post_recipe = PostRecipe.new
+    @foods = Food.all
+    #if @food.genre == "vegetable"
+    #  @foods = Food.find_by(genre: 'vegetable')
+    #end
+  end
+
+  def create
+    @post_recipe = PostRecipe.new(post_recipe_params)
+    @post_recipe.save
+    redirect_to post_recipe_path(@post_recipe.id)
   end
 
   def show
   end
 
   def edit
+  end
+
+  def update
+    @post_recipe = PostRecipe.find(params[:id])
+    @post_recipe.update
+    redirect_to post_recipe_path(@post_recipe.id)
+
+  end
+
+  def destroy
+
+  end
+
+  private
+  def post_recipe_params
+    params.require(:post_recipe).permit(:recipe_image, :title, :advice, :is_draft)
+  end
+
+  def ingredient_params
+    params.require(:ingredient).permit(:serving, :amount, :other_amount)
+  end
+
+  def making_recipe_params
+    params.require(:making_recipe).permit(:recipe)
   end
 end
