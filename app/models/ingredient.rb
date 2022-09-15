1872e4ba0b36096d381510c3cf01dedf0eb524a5
Kurumi-t <kurumisda@gamil.com>
2022-09-15 13:13:24 +0000

    [update]food_nameのバリデーション

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,6 +3,7 @@ class Ingredient < ApplicationRecord
   belongs_to :shopping_list, optional: true
   has_many :foods
 
+  validates :food_name, presence: true
   validates :amount, presence: true
   def self.search(words)
     @ingredient = Ingredient.where("food_name LIKE ?", "%#{words}%")
