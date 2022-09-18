54313c88ca2881c2305a780b397be23840cd9455
Kurumi-t <kurumisda@gamil.com>
2022-09-18 14:08:33 +0000

    [add]コメント機能

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -1,11 +1,8 @@
 class Public::PostCommentsController < ApplicationController
   def index
-    @post_recipe = PostRecipe.find(params[:id])
+    @post_recipe = PostRecipe.find(params[:post_recipe_id])
     @post_comments = @post_recipe.post_comments
-    @post_comment_count = @post_recipe.post_comment.all.count
-  end
-
-  def new
+    @post_comment_count = @post_comments.all.count
     @post_comment = PostComment.new
   end
 
@@ -13,6 +10,7 @@ class Public::PostCommentsController < ApplicationController
     @post_recipe = PostRecipe.find(params[:post_recipe_id])
     @post_comment = PostComment.new(post_comment_params)
     @post_comment.user_id = current_user.id
+    @post_comment.post_recipe_id = @post_recipe.id
     if @post_comment.save
       redirect_to post_recipe_post_comments_path(@post_recipe.id)
     else
