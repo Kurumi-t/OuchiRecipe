582421585d400f45ef9c4ff73a6e67fe1dd1dae6
Kurumi-t <kurumisda@gamil.com>
2022-09-18 14:09:56 +0000

    [add]いいね機能

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -3,13 +3,13 @@ class Public::FavoritesController < ApplicationController
     post_recipe = PostRecipe.find(params[:post_recipe_id])
     favorite = current_user.favorites.new(post_recipe_id: post_recipe.id)
     favorite.save
-    redirect_to post_recipe_path(post_recipe.id)
+    redirect_to post_recipe_path(post_recipe)
   end
 
   def destroy
     post_recipe = PostRecipe.find(params[:post_recipe_id])
-    favorite = current_user.favorites.new(post_recipe_id: post_recipe.id)
+    favorite = current_user.favorites.find_by(post_recipe_id: post_recipe.id)
     favorite.destroy
-    redirect_to post_recipe_path(post_recipe.id)
+    redirect_to post_recipe_path(post_recipe)
   end
 end
