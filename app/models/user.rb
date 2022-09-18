641619ce4b4e958a35292d06f90eff6d8aa4ccb3
Kurumi-t <kurumisda@gamil.com>
2022-09-18 14:07:19 +0000

    [update]画像の変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
 
   def get_profile_image(width, height)
     unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/images/user-no-image.png')
+      file_path = Rails.root.join('app/assets/images/user-no-image.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'profile-default-image.jpg', content_type: 'image/jpeg')
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
