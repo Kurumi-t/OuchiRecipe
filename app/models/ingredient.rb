4d30e9f8c6af07eeb79744c7ac416d4aeff3d45b
Kurumi-t <kurumisda@gamil.com>
2022-09-20 05:53:49 +0000

    [fix]必要のないモデルの削除、バリデーション、アソシエーション変更

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,9 +1,9 @@
 class Ingredient < ApplicationRecord
   belongs_to :post_recipe, optional: true
   belongs_to :shopping_list, optional: true
-  has_many :foods
 
   validates :food_name, presence: true
+  validates :unit, presence: true
   validates :amount, presence: true
   def self.search(words)
     @ingredient = Ingredient.where("food_name LIKE ?", "%#{words}%")
