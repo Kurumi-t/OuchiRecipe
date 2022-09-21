136a82171047c0b965a628c9d4a0f0f5f33ab2a2
Kurumi-t <kurumisda@gamil.com>
2022-09-21 11:10:13 +0000

    [add]退会機能

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -15,6 +15,18 @@ class Public::UsersController < ApplicationController
     redirect_to users_my_page_path
   end
 
+  def withdraw
+    @user = current_user
+  end
+
+  def withdraw_update
+    @user = current_user
+    @user.usspdate(is_deleted: true)
+    reset_session
+    flash[:notice] = "退会しました"
+    redirect_to root_path
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email)
