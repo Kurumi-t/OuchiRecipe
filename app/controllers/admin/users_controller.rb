e4809ab0a9a6cf987ee353d0d8d517a1774bc2c0
Kurumi-t <kurumisda@gamil.com>
2022-09-21 11:08:16 +0000

    [add]管理者のユーザー管理機能

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,19 @@
 class Admin::UsersController < ApplicationController
-  def index
-  end
-
   def show
+    @user = User.page(params[:page])
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
