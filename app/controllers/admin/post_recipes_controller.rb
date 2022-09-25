ecf16ad6803b75a0f6ac054b7487587a9cd5f57d
Kurumi-t <kurumisda@gamil.com>
2022-09-25 10:08:34 +0000

    [add]投稿レシピ管理機能

--- app/controllers/admin/post_recipes_controller.rb
+++ app/controllers/admin/post_recipes_controller.rb
@@ -1,7 +1,20 @@
 class Admin::PostRecipesController < ApplicationController
   def index
+    @post_recipes = PostRecipe.all
   end
 
   def show
+    @post_recipe = PostRecipe.find(params[:id])
+    @ingredients = @post_recipe.ingredients
+    @making_recipes = @post_recipe.making_recipes
+  end
+
+  def destroy
+    post_recipe = PostRecipe.find(params[:id])
+    if post_recipe.destroy
+      redirect_to admin_user_path(post_recipe.user.id)
+    else
+      render :show
+    end
   end
 end
