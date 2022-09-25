f905ab1ef4d19f62855780b5fdfdfb3cb4dca528
Kurumi-t <kurumisda@gamil.com>
2022-09-25 14:29:27 +0000

    [add]バリデーション、enum、アソシエーション設定

--- app/models/fridge.rb
+++ app/models/fridge.rb
@@ -1,2 +1,9 @@
 class Fridge < ApplicationRecord
+  belongs_to :user, optional: true
+  enum genre:  { vegetable: 0, meat: 1, seafood: 2, staple_food_and_flour: 3, eggs_and_milk_and_beans: 4, fruit_and_dessert: 5, seasoning: 6, others: 7 }
+
+  validates :food_name, presence: true
+  validates :genre, presence: true
+  validates :amount, presence: true
+  validates :unit, presence: true
 end
