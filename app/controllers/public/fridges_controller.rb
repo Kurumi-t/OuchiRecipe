3c62d8987fcb0e2f98ec04eff37db4afa9c68a69
Kurumi-t <kurumisda@gamil.com>
2022-09-25 14:29:49 +0000

    [add]冷蔵庫の機能

--- app/controllers/public/fridges_controller.rb
+++ app/controllers/public/fridges_controller.rb
@@ -1,4 +1,44 @@
 class Public::FridgesController < ApplicationController
   def index
+    @user = current_user
+    @fridges = Fridge.all
+    @fridge = Fridge.new
+  end
+
+  def create
+    @user = current_user
+    @fridge = Fridge.new(fridge_params)
+    @fridge.user_id = @user.id
+    if @fridge.save
+      redirect_to fridges_path(@user.id)
+    else
+      @fridges = Fridge.all
+      render :index
+    end
+  end
+
+  def update
+    fridge = Fridge.find(params[:id])
+    if fridge.update(fridge_params)
+      redirect_to fridges_path(current_user.id)
+    else
+      @fridges = Fridge.all
+      render :index
+    end
+  end
+
+  def destroy
+    fridge = Fridge.find(params[:id])
+    if fridge.destroy
+      redirect_to fridges_path(current_user.id)
+    else
+      @fridges = Fridge.all
+      render :index
+    end
+  end
+
+  private
+  def fridge_params
+    params.require(:fridge).permit(:food_name, :genre, :amount, :unit)
   end
 end
