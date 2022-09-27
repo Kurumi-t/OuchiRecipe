b15022108fbd90d36288382eba60c3b7b3cb0e88
Kurumi-t <kurumisda@gamil.com>
2022-09-27 09:02:53 +0000

    [add]検索したキーワードを表示するための記述

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,9 +1,11 @@
 class Public::SearchesController < ApplicationController
   def search
-    @post_recipes = PostRecipe.search(params[:word])
+    @word = params[:word]
+    @post_recipes = PostRecipe.search(@word)
   end
 
   def ingredient_search
-    @post_recipes = PostRecipe.includes(:ingredients).where(ingredients: {food_name: params[:ingredient_name]})
+    @word = params[:ingredient_name]
+    @post_recipes = PostRecipe.includes(:ingredients).where(ingredients: {food_name: @word})
   end
 end
