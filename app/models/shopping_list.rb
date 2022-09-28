2022d2391ee76a91c58c8f27aca0ed4b57ff4a54
Kurumi-t <kurumisda@gamil.com>
2022-09-28 13:56:31 +0000

    [add]アソシエーション、バリデーション設定

--- app/models/shopping_list.rb
+++ app/models/shopping_list.rb
@@ -1,2 +1,7 @@
 class ShoppingList < ApplicationRecord
+  belongs_to :user, optional: true
+
+  validates :food_name, presence: true
+  validates :amount, presence: true
+  validates :unit, presence: true
 end
