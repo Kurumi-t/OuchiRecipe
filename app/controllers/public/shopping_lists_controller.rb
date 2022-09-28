de8f5b68e85b338cabbee9d38d8b73f46ce9c634
Kurumi-t <kurumisda@gamil.com>
2022-09-28 13:57:13 +0000

    [add]買い物リスト機能

--- app/controllers/public/shopping_lists_controller.rb
+++ app/controllers/public/shopping_lists_controller.rb
@@ -1,4 +1,44 @@
 class Public::ShoppingListsController < ApplicationController
   def index
+    @user = current_user
+    @shopping_lists = ShoppingList.all
+    @shopping_list = ShoppingList.new
+  end
+
+  def create
+    @user = current_user
+    @shopping_list = ShoppingList.new(shopping_list_params)
+    @shopping_list.user_id = @user.id
+    if @shopping_list.save
+      redirect_to shopping_lists_path(@user.id)
+    else
+      @shopping_lists = ShoppingList.all
+      render :index
+    end
+  end
+
+  def update
+    shopping_list = ShoppingList.find(params[:id])
+    if shopping_list.update(shopping_list_params)
+      redirect_to shopping_lists_path(current_user.id)
+    else
+      @shopping_lists = ShoppingList.all
+      render :index
+    end
+  end
+
+  def destroy
+    shopping_list = ShoppingList.find(params[:id])
+    if shopping_list.destroy
+      redirect_to shopping_lists_path(current_user.id)
+    else
+      @shopping_lists = ShoppingList.all
+      render :index
+    end
+  end
+
+  private
+  def shopping_list_params
+    params.require(:shopping_list).permit(:food_name, :amount, :unit)
   end
 end
