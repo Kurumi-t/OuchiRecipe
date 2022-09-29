8a7b7d090f3514b5462a63f3928475524aed9509
Kurumi-t <kurumisda@gamil.com>
2022-09-29 15:43:28 +0000

    [update]destroy_allアクション

--- app/controllers/public/fridges_controller.rb
+++ app/controllers/public/fridges_controller.rb
@@ -33,6 +33,31 @@ class Public::FridgesController < ApplicationController
     end
   end
 
+  def fridge_create
+    @fridge = Fridge.new(fridge_params)
+    shopping_lists = current_user.shopping_lists.all
+    shopping_lists.each do |shopping_lists|
+      shopping_list.food_name = @fridge.food_name
+      shopping_list.amount = @fridge.amount
+      shopping_list.unit = @fridge.unit
+    end
+    if @fridge.save
+      shopping_lists.destroy_all
+      redirect_to fridges_path(@user.id)
+    else
+      @fridges = Fridge.all
+      @vegetable_fridges = Fridge.where(user_id: @user.id, genre: 0)
+      @meat_fridges = Fridge.where(user_id: @user.id, genre: 1)
+      @seafood_fridges = Fridge.where(user_id: @user.id, genre: 2)
+      @staple_food_and_flour_fridges = Fridge.where(user_id: @user.id, genre: 3)
+      @eggs_and_milk_and_beans_fridges = Fridge.where(user_id: @user.id, genre: 4)
+      @fruit_and_dessert_fridges = Fridge.where(user_id: @user.id, genre: 5)
+      @seasoning_fridges = Fridge.where(user_id: @user.id, genre: 6)
+      @others_fridges = Fridge.where(user_id: @user.id, genre: 7)
+      render :index
+    end
+  end
+
   def update
     fridge = Fridge.find(params[:id])
     if fridge.update(fridge_params)
