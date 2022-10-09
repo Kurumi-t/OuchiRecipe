fdab5b02b4e0aca2dd4ab729c5279d4d525ca2b3
Kurumi-t <kurumisda@gamil.com>
2022-10-09 14:05:15 +0000

    [add]font awesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ import "channels"
 import "popper.js";
 import "bootstrap";
 import "../stylesheets/application"
+import '@fortawesome/fontawesome-free/js/all';
 
 require('jquery')
 require('cocoon')
