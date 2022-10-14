9d5e108ba41b567c13c6ba387104120f273be757
Kurumi-t <kurumisda@gamil.com>
2022-10-14 10:39:06 +0000

    [fix]コメント件数

--- app/controllers/admin/post_comments_controller.rb
+++ app/controllers/admin/post_comments_controller.rb
@@ -1,14 +1,16 @@
 class Admin::PostCommentsController < ApplicationController
   def index
     @post_recipe = PostRecipe.find(params[:post_recipe_id])
-    @post_comments = @post_recipe.post_comments
-    @post_comment_count = @post_comments.all.count
+    post_comments = @post_recipe.post_comments
+    @post_comments = post_comments.page(params[:page]).per(10)
+    @post_comment_count = post_comments.all.count
   end
 
   def show
     @user = User.find(params[:user_id])
-    @post_comments = @user.post_comments
-    @post_comment_count = @post_comments.all.count
+    post_comments = @user.post_comments
+    @post_comments = post_comments.page(params[:page]).per(10)
+    @post_comment_count = post_comments.all.count
   end
 
   def destroy
