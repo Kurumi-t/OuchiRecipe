ee1fb701f41c9c53891b381149f860ff02b41271
Kurumi-t <kurumisda@gmail.com>
2022-10-21 12:54:18 +0000

    [fix]環境変更によるホスト変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,5 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.hosts << "56ee0f794c544587bb435c1d89729047.vfs.cloud9.us-east-1.amazonaws.com"
+  config.hosts << "b04844f01fe247e1bc1ad5012c6f066a.vfs.cloud9.us-east-1.amazonaws.com"
 end
